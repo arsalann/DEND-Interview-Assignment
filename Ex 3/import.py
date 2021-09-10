4e3881a96c06f91b28ccd50c150dcc37f6930f35
Arsalan Noorafkan <arsalan@atlys.ca>
2021-09-10 10:42:35 -0400

    exercise 1

--- Ex 3/import.py
+++ Ex 3/import.py
@@ -1,6 +1,7 @@
+import pandas
 import pyspark as spark
+# import sqlalchemy
+# pip install pyspark
+# pip install pandas
+# pip install sqlalchemy 
 
-
-
-
-https://climate.weather.gc.ca/climate_data/bulk_data_e.html?format=csv&stationID=14&Year=2020&Month=11&Day=1&timeframe=2&submit=Download+Data
--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # DEND-Interview-Assignment
  
+
+
+
+
+# Chose to use DLY (Daily) aggregated data since the analysis required comparing a single day to previous years.
